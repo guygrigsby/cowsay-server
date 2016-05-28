a46e01ea2e23f6926a79e2b9dcc596b8ae902990
Guy Grigsby (Mac) <guy.grigsby@gmail.com>
2016-05-28 09:31:29 -0600

    omit empty tokens in conf

--- config.go
+++ config.go
@@ -1,7 +1,7 @@
 package main
 
 type Config struct {
-	Tokens   []string `json:"tokens"`
+	Tokens   []string `json:"tokens,omitempty"`
 	CertFile string   `json:"certfile"`
 	KeyFile  string   `json:"keyfile"`
 	ListenOn string   `json:"listenon"`
